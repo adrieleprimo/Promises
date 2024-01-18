d5f8b78e4a75d4712b2d04074051e2a1c4a62391
Adriele <adriele.dejprimo@gmail.com>
2024-01-18 17:06:00 -0300

    Add the cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const router = require('./router');
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
