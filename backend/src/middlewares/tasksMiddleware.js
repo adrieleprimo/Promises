005a4c5f83cf714fd756fc5b08d5831202e99e91
Adriele <adriele.dejprimo@gmail.com>
2024-01-18 15:14:32 -0300

    Add a middleware about field status

--- backend/src/middlewares/tasksMiddleware.js
+++ backend/src/middlewares/tasksMiddleware.js
@@ -1,4 +1,4 @@
-const validateBody = async(req, res, next)=>{
+const validateFieldTitle = async(req, res, next)=>{
     const {body} = req;
 
     if(body.title === undefined){
@@ -10,6 +10,20 @@ const validateBody = async(req, res, next)=>{
     next();
 };
 
+const validateFieldStatus = async(req, res, next)=>{
+    const {body} = req;
+
+    if(body.status === undefined){
+        return res.status(400).json({message: 'The field status is required'});
+    }
+    if(body.status === ''){
+        return res.status(400).json({message:'status cannot be empty'});
+    }
+    next();
+};
+
+
 module.exports = {
-    validateBody
+    validateFieldTitle,
+    validateFieldStatus
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -4,9 +4,12 @@ const tasksController = require('./controllers/tasksControllers');
 const tasksMiddleware = require('./middlewares/tasksMiddleware');
 
 router.get('/tasks', tasksController.getAll);
-router.post('/tasks', tasksMiddleware.validateBody ,tasksController.createTask);
+router.post('/tasks', tasksMiddleware.validateFieldTitle ,tasksController.createTask);
 router.delete('/tasks/:id', tasksController.deleteTask);
-router.put('/tasks/:id', tasksController.updateTask);
+router.put('/tasks/:id',
+tasksMiddleware.validateFieldTitle,
+tasksMiddleware.validateFieldStatus,
+tasksController.updateTask);
 
 module.exports = router;
 
