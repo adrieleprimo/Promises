6d5886499d513f6525dde5da65d5e47cfce3a88f
Adriele <adriele.dejprimo@gmail.com>
2024-01-23 16:17:47 -0300

    Fixing the problem about the deletePromises

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,9 +1,35 @@
 const tbody = document.querySelector('tbody');
+const addForm = document.querySelector('.add-form');
+const inputPromise = document.querySelector('.input-promise');
 
 const fetchPromises = async ()=>{
     const response = await fetch('http://localhost:8081/promises');
     const promises = await response.json();
     return promises; 
+}
+
+const addPromise = async(event)=>{
+    event.preventDefault(); 
+
+    const promise = {title:inputPromise.value}
+
+    await fetch('http://localhost:8081/promises',{
+        method:'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(promise)
+    });
+    loadPromises();
+    inputPromise.value = '';
+}
+
+const deletePromise = async(id)=>{
+    await fetch(`http://localhost:8081/promises/${id}`, {
+        method: 'DELETE',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+    });
+    loadPromises();
 }
 
 const createElement = (tag, innerText = '', innerHTML = '')  =>{
@@ -23,7 +49,7 @@ const createRow =  (promise) =>{
 
     const tr = document.createElement('tr');
     const tdTitle = createElement('td', title);
-    const tdCreated_at = createElement('td', created_at);
+    const tdCreated_at = createElement('td', formatDate(created_at));
     const tdStatus = createElement('td');
     const tdActions = createElement('td');
     
@@ -33,6 +59,8 @@ const createRow =  (promise) =>{
     const deleteButton = createElement('button', '','<span class="material-symbols-outlined"> delete_forever </span>');
     editButton.classList.add('btn-action');
     deleteButton.classList.add('btn-action');
+
+    deleteButton.addEventListener('click', ()=>deletePromise(id));
 
     tdStatus.appendChild(select);
 
@@ -57,13 +85,21 @@ const createSelect = (value)=>{
     return select;
 }
 
-const loadPromises = async ()=>{
+const loadPromises = async()=>{
     const promises = await fetchPromises();
+    tbody.innerHTML = '';
 
     promises.forEach((promise)=>{
-
+        const tr = createRow(promise);
+        tbody.appendChild(tr);
     });
-    }
+}
 
-createRow(task);
+const formatDate = (dateUTC)=>{
+    const options = {dateStyle: 'long', timeStyle: 'short'};
+    const date = new Date(dateUTC).toLocaleString('en',options);
+    return date;
+}
 
+addForm.addEventListener('submit', addPromise);
+loadPromises();
