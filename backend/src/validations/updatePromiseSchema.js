078ad28296f1ad6c75edfcef61874841a66ca747
Adriele <adriele.dejprimo@gmail.com>
2024-02-10 10:47:23 -0300

    Fixing some problems about title empty

--- backend/src/validations/updatePromiseSchema.js
+++ backend/src/validations/updatePromiseSchema.js
@@ -2,15 +2,15 @@ const joi  = require('joi');
 
 const updatePromiseSchema = joi.object({
     title:joi.string().required().trim().messages({
-        'any.required': 'the field title is required',
-        'string.empty': 'the field title is required',
+        'any.required': 'The title field is required',
+        'string.empty': 'the title field is required',
         'string.base': 'the title field must to be a string'
-    }),
-    status:joi.string().trim().message({
-        'any.required': 'the field status is required',
-        'string.empty': 'the field status is required',
+    }).optional().options({convert: false}),
+    status:joi.string().required().trim().message({
+        'any.required': 'The status field is required',
+        'string.empty': 'the status field is required',
         'string.base': 'the status field must to be a string'
-    })
+    }).optional().options({convert: false})
 });
 
 module.exports = updatePromiseSchema;
